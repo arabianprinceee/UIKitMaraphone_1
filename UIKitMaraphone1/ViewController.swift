d66fe519dcefe033720cd5dd9cfaab11bc47158b
Anas Ben Mustafa <anas.mustafa2301@gmail.com>
2024-02-03 12:19:01 +0300

    Initial commit

--- UIKitMaraphone1/ViewController.swift
+++ UIKitMaraphone1/ViewController.swift
@@ -7,13 +7,69 @@
 
 import UIKit
 
+final class Square: UIView {
+    
+    private let gradientLayer = CAGradientLayer()
+    
+    init() {
+        super.init(frame: .zero)
+        setup()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    override func layoutSubviews() {
+        gradientLayer.frame = bounds
+    }
+    
+    private func setup() {
+        layer.cornerRadius = 16
+        backgroundColor = .gray
+        setupGradient()
+        setupShadow()
+        setupLayout()
+    }
+    
+    private func setupGradient() {
+        gradientLayer.cornerRadius = layer.cornerRadius
+        gradientLayer.colors = [UIColor.systemPurple.cgColor, UIColor.red.cgColor]
+        gradientLayer.startPoint = CGPoint(x: 0, y: 0)
+        gradientLayer.endPoint = CGPoint(x: 1, y: 1)
+        layer.addSublayer(gradientLayer)
+    }
+    
+    private func setupShadow() {
+        layer.shadowColor = UIColor.black.cgColor
+        layer.shadowOpacity = 0.5
+        layer.shadowOffset = CGSize(width: 1, height: 5)
+        layer.shadowRadius = 5
+    }
+    
+    private func setupLayout() {
+        translatesAutoresizingMaskIntoConstraints = false
+        NSLayoutConstraint.activate([
+            heightAnchor.constraint(equalToConstant: 100),
+            widthAnchor.constraint(equalToConstant: 100)
+        ])
+    }
+    
+}
+
 class ViewController: UIViewController {
+    
+    let square = Square()
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        view.addSubview(square)
+        NSLayoutConstraint.activate([
+            square.centerYAnchor.constraint(equalTo: view.centerYAnchor),
+            square.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 100)
+        ])
     }
-
 
 }
 
